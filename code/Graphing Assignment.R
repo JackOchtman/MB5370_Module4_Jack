99286fe2277757fc04d06828b8054ca24541fb4f
Jack Ochtman <spartanjack017@gmail.com>
2025-04-18 17:48:14 +1000

    comment fix

--- code/Graphing Assignment.R
+++ code/Graphing Assignment.R
@@ -1,15 +1,21 @@
 
 
+# Install and load tidyverse and ggplot packages, run install lines only if needed.
+#install.packages("tidyverse")
+library("tidyverse")
+#install.packages("ggplot2")
+library("ggplot2")
 
-data <- read.csv("E:/MB5370/MB5370_Module4_Jack/data/Extracted_Fish_Population_Data.csv")
+#Adds the dataframe
+
+df <- read_csv("C:/Users/Spart/Documents/Github/MB5370_Module4_Jack/data/Extracted_Fish_Population_Data.csv")
 
 library(ggplot2)
 
 # Create the plot
-ggplot(data, aes(x = Days.from.Unification, y = Number.of.Fish, color = Species)) +
+ggplot(df, aes(x = Days.from.Unification, y = Number.of.Fish, color = Species)) +
   geom_smooth(method = "loess", se = FALSE) +  # Smoothed line without confidence interval
   labs(title = "Fish Species Growth Over Days of Unification",
        x = "Days from Unification",
        y = "Number of Fish") +
   theme_minimal()  # Optional theme
-####test
